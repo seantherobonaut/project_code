804ecd236b1f06458277bda6f9d93ba2aab98891
Sean Leapley <seantherobonaut@gmail.com>
2023-11-03 10:15:39 -0700

    save progress for asyncjs

--- Web/LearningNode/AsyncJS/public/js/script.js
+++ Web/LearningNode/AsyncJS/public/js/script.js
@@ -1,3 +1,66 @@
+// const getTodos = (resource, callback) => 
+// {
+//     const request = new XMLHttpRequest();
+
+//     request.addEventListener('readystatechange', ()=>
+//     {
+//         if(request.readyState === 4 && request.status === 200)
+//         {
+//             const data = JSON.parse(request.responseText);
+//             callback(undefined, data);
+//         }
+//         else
+//         {
+//             if(request.readyState === 4)
+//             {
+//                 callback('could not fetch data', undefined);
+//             }
+//         }
+//     });
+
+//     request.open('GET', resource);
+//     request.send(null);
+// };
+
+// //Callback Hell, do NOT do this!
+// getTodos('/todos/file1.json', (error, data)=>{
+//     console.log(data);
+//     getTodos('/todos/file2.json', (error, data)=>{
+//         console.log(data);
+//         getTodos('/todos/file3.json', (error, data)=>{
+//             console.log(data);
+//         });
+//     });
+// });
+
+
+
+//the data we pass to the resolve function is then returned and passed to the callback function in the .then() function
+
+
+
+
+
+
+const getSomething = ()=>
+{
+    //either returns success or reject .then() or .catch()
+    return new Promise((resolve, reject)=>
+    {
+        //fetch something
+        // resolve('some data');
+        reject('some error');
+    });
+};
+
+getSomething().then((data)=>
+{
+    console.log(data);
+}).catch((error)=>
+{
+    console.log(error);
+});
+
 // fetch('https://jsonplaceholder.typicode.com/todos').then(response => response.json()).then((json) =>
 // {
 //     const element = document.getElementById("target");
@@ -15,63 +78,3 @@
 //         element.innerHTML += jsonstring+'<br>]';
 //     }
 // });
-
-
-const getTodos = (callback) => 
-{
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', ()=>
-    {
-        if(request.readyState === 4 && request.status === 200)
-        {
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        }
-        else
-        {
-            if(request.readyState === 4)
-            {
-                callback('could not fetch data', undefined);
-            }
-        }
-    });
-
-    request.open('GET', '/todos/file3.json');
-    request.send(null);
-};
-
-
-getTodos((error, data)=>
-{
-    console.log('callback fired');
-    if(error)
-    {
-        console.log(error);
-    }
-    else
-    {
-        console.log(data);
-    }
-});
-
-
-// const getSomething = ()=>
-// {
-//     //either returns success or reject .then() or .catch()
-//     return new Promise((resolve, reject)=>
-//     {
-//         //fetch something
-//         // resolve('some data');
-//         reject('some error');
-//     });
-// };
-
-// getSomething().then((data)=>
-// {
-//     console.log(data);
-// }).catch((error)=>
-// {
-//     console.log(error);
-// });
-
